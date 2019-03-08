a61762833443db7982cbd0ee80b224557b134ae8
Mark Zolton <mzolton@jccc.edu>
2019-03-08 15:15:17 -0600

    Added prototype for JSON bookmarks.

--- js/main.js
+++ js/main.js
@@ -1,46 +1,57 @@
-/** Paralax scrolling and blur for background image. */
-$(window).scroll(function(){
-  $(".blurParallaxBg__image").css("opacity", 1 - $(window).scrollTop() / 500);
-  $(".blurParallaxBg__image").css("top", 1 - $(window).scrollTop() / 4);
+/* Prototype JSON for bookmarks. */
+window.bookmarkData = {
+  "key": "value",
+  "bookmarks": [
+    {  "href": "https://www.primagames.com/games/elder-scrolls-online/guides/elder-scrolls-online-morrowind-eguide",
+      "image": "img/bookmarks/TESO-Morrowind-01-HD.png",
+      "title": "The Elder Scrolls Online: Morrowind eGuide" 
+    },
+    {  "href": "https://www.primagames.com/games/fallout-76/guides/fallout-76-eguide",
+      "image": "img/bookmarks/Fallout-76-05-HD.png",
+      "title": "Fallout 76 eGuide" 
+    },
+    {  "href": "http://deltiasgaming.com/eso-stamina-nightblade-dps-build/",
+      "image": "",
+      "title": "ESO Stamina Nightblade DPS Build" 
+    },
+    {  "href": "http://eso-skills.com/",
+      "image": "",
+      "title": "ESO Skill Calculator" 
+    }
+  ]
+};
+
+$( document ).ready(function() {
+
+  /* Paralax scrolling and blur for background image. */
+  $(window).scroll(function(){
+    $(".blurParallaxBg__image").css("opacity", 1 - $(window).scrollTop() / 500);
+    $(".blurParallaxBg__image").css("top", 1 - $(window).scrollTop() / 4);
+  });
+
+  /* Populate bookmarks */
+  if(!$.isEmptyObject(window.bookmarkData)) {
+    var bookmarkHtml = '';
+    var bookmarkElement = document.getElementById("bookmarks");
+    var bookmarks = window.bookmarkData.bookmarks;
+
+    // For each bookmark in bookmarks ...
+    bookmarks.forEach(function(bookmark) {
+     
+      // The bookmark template.
+      var bookmarkTemplate = `
+        <div class="grid__item">
+          <a class="bookmark aspect-ratio-box" href="${bookmark.href}" style="background-image: url('${bookmark.image}')">
+            <div class="bookmark__content">
+              <div class="bookmark__title">${bookmark.title}</div>
+            </div>
+          </a>
+        </div>
+      `;  
+
+      bookmarkHtml += bookmarkTemplate;
+    });
+
+    bookmarkElement.innerHTML = bookmarkHtml;
+  }
 });
-
-/*
-var clockElement = document.getElementById("clock");
-
-function updateClock(clock) {
-    // clock.innerHTML = new Date().toTimeString().split(" ")[0];
-    clock.innerHTML = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true}).toLowerCase();
-}
-
-setInterval(function () {
-    updateClock(clockElement);
-}, 1000);
-
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var d = new Date();
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-
-var n = weekday[d.getDay()];
-var yyyy = today.getFullYear();
-
-if (dd < 10) {
-    dd = '0' + dd
-}
-
-if (mm < 10) {
-    mm = '0' + mm
-}
-
-today = mm + '.' + dd + '.' + yyyy;
-
-date.innerHTML = n + "<br>" + today;
-*/
